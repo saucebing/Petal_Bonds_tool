c92eee895c682059343631c7d3b0650cba3099f1
Paimon2022 <Paimon_2022@hotmail.com>
2024-06-05 14:38:53 +0800

    modify run_docker.sh image version

--- server_tool/run_docker.sh
+++ server_tool/run_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --privileged --name petal_bonds --net=host -v /server:/server -v /data:/data -dit petalbonds:v0.3 /usr/sbin/init
+docker run --privileged --name petal_bonds --net=host -v /server:/server -v /data:/data -dit petalbonds:v0.4 /usr/sbin/init
